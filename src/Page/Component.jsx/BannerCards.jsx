e432cdec4dee3c525fe5117f5c5c2f51df1fe62d
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-07-24 11:28:32 +0530

    section 2 has been completed

--- src/Page/Component.jsx/BannerCards.jsx
+++ src/Page/Component.jsx/BannerCards.jsx
@@ -2,17 +2,17 @@ import { generalIcons } from "../../assets";
 
 export default function BannerCard({ icon, text }) {
     return (
-        <div className="border border-1 border-[#F95017] w-[20vw] rounded-lg bg-gradient-to-r from-[#FDC830] to-[#F37335] p-0">
+        <div className="border border-1 border-[#F95017] w-full sm:w-[20vw] rounded-lg bg-gradient-to-r from-[#FDC830] to-[#F37335] p-0">
 
             <div className="bg-white/90 rounded-lg p-[10px]">
 
-                <div className="border-b border-[#FF9B6390] flex flex-col justify-between h-[140px]">
+                <div className="border-b border-[#FF9B6390] flex gap-[12px] pb-[20px] sm:pb-[0px] sm:gap-[0px] sm:flex-col justify-between sm:h-[140px]">
 
                     <span className="w-[50px] !h-[50px] flex justify-center items-center rounded-full bg-[#F5843720]">
                         <img src={icon} alt={"icons"} className="w-[29px] h-[29px]" />
                     </span>
 
-                    <p className="text-gray-800 text-[clamp(12px,1.5vw,18px)] flex-[1] font-[500] flex items-center ">
+                    <p className="text-gray-800 sm:text-[clamp(12px,1.5vw,18px)] text-[clamp(13px,4vw,16px)] flex-[1] font-[500] flex items-center ">
                         {text || ""}
                     </p>
                 </div>
--- src/Page/LandingPage.jsx
+++ src/Page/LandingPage.jsx
@@ -12,7 +12,7 @@ import { FAQ } from "./Component.jsx/FAQ";
 import { Footer } from "./Component.jsx/Footer";
 
 function LandingPage() {
-    const px = 'px-[clamp(60px,8.3vw,120px)]'
+    const px = 'sm:px-[clamp(60px,8.3vw,120px)] px-[14px]'
     const py = 'py-[clamp(40px,5.76vw,85px)]'
 
 
@@ -23,14 +23,14 @@ function LandingPage() {
             <Header />
 
             {/* banner */}
-            <section className="pt-20 px-[120px] border h-screen flex flex-col justify-center items-center gap-[15px]">
+            <section className={`pt-[120px] pb-20 ${px} border  flex flex-col justify-center items-center gap-[15px]`}>
                 <h1 className="text-[clamp(28px,3vw,42px)] font-[500] text-center">
                     MiloFuel: A Better Way to Purchase Fuel Oil
                 </h1>
-                <p className=" text-[18px] font-[300] pb-[50px] w-[676px] text-center">
+                <p className=" text-[18px] font-[300] pb-[50px] sm:w-[676px] text-center">
                     Convenient Fuel Buying made smarter, easier and more reliable — without the hassle of constant monitoring or comparison shopping.
                 </p>
-                <div className="flex gap-[15px]">
+                <div className="flex gap-[15px] flex-col sm:flex-row">
                     {
                         BannerCards?.map((element, index) => {
                             return (
@@ -39,14 +39,14 @@ function LandingPage() {
                         })
                     }
                 </div>
-                <ButtonFuel text={"Buy Fuel Now"} style={"w-[clamp(150px,14vw,213px)] text-[clamp(14px,1.4vw,20px)] h-[clamp(40px,10vh,80px)] mt-[60px] px-[clamp(6px,2.5vw,24px)]"} />
+                <ButtonFuel text={"Buy Fuel Now"} style={"hidden sm:block w-[clamp(150px,14vw,213px)] text-[clamp(14px,1.4vw,20px)] h-[clamp(40px,10vh,80px)] mt-[60px] px-[clamp(6px,2.5vw,24px)]"} />
             </section>
 
-            <section className={`bg-gradient-to-r from-[#FDC83020] to-[#F3733520] py-[60px] h-screen flex flex-col lg:flex-row h-fit justify-between items-center px-[122px]`}>
-                <div className="w-[336px]">
+            <section className={`bg-gradient-to-r from-[#FDC83020] to-[#F3733520] py-[60px] ${px} flex flex-col sm:flex-row h-fit justify-between items-center`}>
+                <div className="sm:w-[336px] w-full">
                     <div className="flex flex-col gap-[20px]">
                         <h2 className="text-[clamp(30px,3vw,45px)] font-[600]">Milo, the Mule</h2>
-                        <p className="leading-[34px] font-[300] text-[clamp(13px,1.3vw,19.4px)]">Your smart fuel assistant that keeps you informed, efficient, and always in control.</p>
+                        <p className="leading-[34px] font-[300] sm:text-[clamp(13px,1.3vw,19.4px)] text-[16px]">Your smart fuel assistant that keeps you informed, efficient, and always in control.</p>
 
                         {
                             Section2?.map((element, i) => {
@@ -62,14 +62,14 @@ function LandingPage() {
                             })
                         }
                     </div>
-                    <ButtonFuel text={"Tell Me More"} style={"mt-[47px] h-[80px] w-[213px] text-[20px] font-[500] hidden lg:block"} />
+                    <ButtonFuel text={"Tell Me More"} style={"mt-[47px] h-[80px] w-[213px] text-[20px] font-[500] hidden sm:block"} />
 
                 </div>
 
-                <div className=" w-[790px] h-[606px] flex justify-center align-center relative ">
-                    <span className="block w-[408px] h-[408px] absolute top-[15%]  bg-gradient-to-r from-[#FF690F39] to-[#E8381B39] rounded-full"></span>
-                    <img src={image.rocket} alt="" className="z-[1] w-[261px] h-[434px] " />
-                    <div className="w-full absolute top-[15%] grid grid-cols-2 gap-x-[274px] auto-rows-fr justify-center items-center">
+                <div className=" sm:w-[clamp(350px,54vw,790px)] w-[clamp(290px,90vw,790px)] sm:h-[606px] [@media(max-width:400px)]:h-[70vh] flex justify-center align-center relative ">
+                    <span className="block sm:w-[408px] w-[269px] aspect-[1/1] absolute top-[15%]  bg-gradient-to-r from-[#FF690F39] to-[#E8381B39] rounded-full"></span>
+                    <img src={image.rocket} alt="" className="z-[1] sm:w-[261px] w-[170px] sm:h-[434px] h-[283px]" />
+                    <div className="w-full absolute top-[49%] [@media(min-width:400px)]:top-[15%] grid grid-cols-2 sm:gap-x-[clamp(60px,10vw,274px)] md:gap-x-[clamp(60px,16vw,274px)] gap-x-[2px] z-[3] auto-rows-fr  justify-center items-center">
                         {rocketCards?.map((element, i) => {
                             const total = rocketCards.length;
                             const isFirstRow = i < 2;
@@ -80,22 +80,22 @@ function LandingPage() {
 
                             let marginTop = "";
                             if (!isFirstRow) {
-                                marginTop = isLastRow ? "mt-[60px]" : "mt-[70px]";
+                                marginTop = isLastRow ? "lg:mt-[60px] md:mt-[-30px] sm:mt-[-40px] mt-[30px]" : "sm:mt-[clamp(30px,4.8vw,70px)]";
                             }
 
                             return (
                                 <div
                                     key={i}
-                                    className={`h-[101px]
-                                                py-[21px] px-[17px] flex flex-col gap-[6px]
+                                    className={`lg:h-[101px] 
+                                                sm:py-[21px] py-[10px] sm:px-[17px] px-[8px] flex flex-col gap-[6px]
                                                 bg-gradient-to-r from-[#FEF1EC] via-[#F5843750] to-[#F5843750]
                                                 rounded-[12px] 
                                                 ${isLonelyLast ? "col-span-2 justify-self-center" : ""}
                                                 ${marginTop || "mt-[-10px]"}
                                             `}
                                 >
-                                    <h3 className="font-[700] text-[13px]">{element.heading}</h3>
-                                    <p className="text-[11px]">{element.para}</p>
+                                    <h3 className="font-[700] sm:text-[13px] text-[11px]">{element.heading}</h3>
+                                    <p className="sm:text-[11px] text-[10px]">{element.para}</p>
                                 </div>
                             );
                         })}
@@ -105,7 +105,7 @@ function LandingPage() {
 
             </section>
 
-            <section className="px-[120px] py-[80px] flex flex-col justify-center items-center text-center">
+            {/* <section className="px-[120px] py-[80px] flex flex-col justify-center items-center text-center">
                 <div>
                     <div>
                         <h2 className="text-[42px] font-[500]">Fuel Delivery Options</h2>
@@ -173,18 +173,18 @@ function LandingPage() {
                         ></div>
                     </div>
                 </div>
-            </section>
+            </section> */}
 
-            <section className="bg-black px-[clamp(60px,8.3vw,120px)] py-[clamp(40px,5.76vw,85px)] flex">
+            {/* <section className="bg-black px-[clamp(60px,8.3vw,120px)] py-[clamp(40px,5.76vw,85px)] flex">
                 <div className="text-white w-[clamp(400px,43vw,632px)]">
                     <h2 className="font-[600] text-[clamp(30px,3vw,42px)]">Explore Endless Possibilities with Smarter Fueling</h2>
                     <p className="font-[400] text-[18px] w-[clamp(200px,34vw,496px)]">Smarter fueling with real-time insights, instant payments, smart refills, industry-leading security, and total control—right at your fingertips.</p>
                     <ButtonFuel text={"Buy Fuel Now"} style={"mt-[47px] h-[80px] w-[213px] text-[20px] font-[500] hidden lg:block text-[black]"} />
                 </div>
                 <div className=" w-[clamp(400px,43vw,632px)]"></div>
-            </section>
+            </section> */}
 
-            <section className={`${px} ${py} `}>
+            {/* <section className={`${px} ${py} `}>
                 <div className="text-center">
                     <h2 className={`font-[500] text-[42px]`}>Fuel Your Business with Real-Time Intelligence</h2>
                     <p className={`font-[300] text-[18px]`}>Track, analyze, and optimize your fuel operations — all in one powerful platform.</p>
@@ -221,9 +221,9 @@ function LandingPage() {
                         })}
                     </div>
                 </div>
-            </section>
+            </section> */}
 
-            <section className={`bg-gradient-to-r from-[#FDC83030] to-[#F3733530] ${px} ${py} flex gap-[64px]`}>
+            {/* <section className={`bg-gradient-to-r from-[#FDC83030] to-[#F3733530] ${px} ${py} flex gap-[64px]`}>
                 <div>
                     <img src={image.content} alt="" className={`w-[581px]`} />
                 </div>
@@ -238,7 +238,6 @@ function LandingPage() {
 
                             return (
                                 <div key={i} className="relative pl-6 flex items-center">
-                                    {/* Span with color and line */}
                                     <span
                                         className={`
                                             relative font-bold z-10  block w-[50px] h-[50px]
@@ -248,7 +247,6 @@ function LandingPage() {
                                     >
                                         {i + 1}
 
-                                        {/* Line going down from this span to the next */}
                                         {!isLast && (
                                             <span
                                                 className={`
@@ -259,7 +257,6 @@ function LandingPage() {
                                         )}
                                     </span>
 
-                                    {/* Text content */}
                                     <p className="ml-2">{element.text}</p>
                                 </div>
                             );
@@ -269,15 +266,15 @@ function LandingPage() {
 
                 </div>
 
-            </section>
+            </section> */}
 
-            <section className="py-16">
+            {/* <section className="py-16">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <FAQ />
                 </div>
-            </section>
+            </section> */}
 
-            <Footer />
+            {/* <Footer /> */}
         </main >
     );
 }
